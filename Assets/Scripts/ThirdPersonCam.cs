5e95ee4f8f04c7600dcca7b8f1786b7cafe8a9dc
Dylan <dylanosbornebuisness@gmail.com>
2023-11-04 11:53:32 +0200

    Complete game revamp, decided to move the game idea from a rocket league clone to a completely separate game idea, new game idea is to still have a football game but make robot characters instead of cars and impletement own mechanics for the robots

---
+++ Assets/Scripts/ThirdPersonCam.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using TMPro;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform player; // Reference to the player
+
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+
+    }
+}
