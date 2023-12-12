4e408457f81c5e02daba6df96528adc462b28b2d
Dylan <dylanosbornebuisness@gmail.com>
2023-12-12 16:02:17 +0200

    Changes made to the game UI

--- Assets/Scripts/Game Scripts/UIManager.cs
+++ Assets/Scripts/Game Scripts/UIManager.cs
@@ -4,6 +4,8 @@ using System;
 
 public class UIManager : MonoBehaviour
 {
+    public RectTransform canvasRect;
+
     [SerializeField] private TextMeshProUGUI staminaText;
     [SerializeField] private TextMeshProUGUI timeText;
     [SerializeField] private TextMeshProUGUI rGoalText;
@@ -17,6 +19,12 @@ public class UIManager : MonoBehaviour
         // Initialize default values
         timeText.text = "05:00";
         staminaText.text = "100";
+
+        // Initialize text placements using percentages
+        /*SetTextPosition(staminaText.rectTransform, 0.9f, 0.2f);
+        SetTextPosition(timeText.rectTransform, 0.5f, 0.8f);
+        SetTextPosition(rGoalText.rectTransform, 0.4f, 0.8f);
+        SetTextPosition(bGoalText.rectTransform, 0.6f, 0.8f);*/
     }
 
     void Update()
@@ -28,5 +36,19 @@ public class UIManager : MonoBehaviour
 
         rGoalText.text = stateMachine.redGoals.ToString();
         bGoalText.text = stateMachine.blueGoals.ToString();
+    }
+
+    void SetTextPosition(RectTransform textTransform, float xPercent, float yPercent)
+    {
+        // Get the canvas rect size
+        float canvasWidth = canvasRect.rect.width;
+        float canvasHeight = canvasRect.rect.height;
+
+        // Calculate position based on percentage
+        float xPos = canvasWidth * xPercent;
+        float yPos = canvasHeight * yPercent;
+
+        // Set the anchored position
+        textTransform.anchoredPosition = new Vector2(xPos, yPos);
     }
 }
